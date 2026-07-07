cbaa0efca9c5b36b950eba2a161d9d22ea3bbd0a
mordenfarm <mordenfarm1677@gmail.com>
2026-07-07 15:09:18 +0200

    Make Firebase database URL optional

--- firebase.ts
+++ firebase.ts
@@ -5,14 +5,25 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  ...(import.meta.env.VITE_FIREBASE_DATABASE_URL
+    ? { databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL }
+    : {}),
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-const missingFirebaseConfig = Object.entries(firebaseConfig)
+const requiredFirebaseConfig = {
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId,
+};
+
+const missingFirebaseConfig = Object.entries(requiredFirebaseConfig)
   .filter(([, value]) => !value)
   .map(([key]) => key);
 
